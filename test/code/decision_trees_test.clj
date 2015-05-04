08dbe51d3a4de6483a9db80b48710cd72dc5e8c3
Gabriel Ferns <exclamaforte@gmail.com>
2015-05-04 04:35:45 -0500

    renamed some files and added one test to trees

--- test/code/decision_trees_test.clj
+++
@@ -1,9 +0,0 @@
-(ns skew.trees-testb
-  (:require [clojure.test.check :as tc]
-            [clojure.test.check.generators :as gen]
-            [clojure.test.check.properties :as prop]
-            [clojure.test.check.clojure-test :as ct]
-            [code.decision-trees :as dt]
-            [clojure.set]))
-
-(conditional-entropy (core/nth-attribute (core/nth-instance core/test-instances 1) 0) core/test-instances)
